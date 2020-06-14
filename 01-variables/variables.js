69e5e0121d8d0758451490b2e14ff2adb39e5812
DekimXD <miguelclementeaguilardiaz@hotmail.com>
2020-06-14 16:30:13 -0500

    variables

---
+++ 01-variables/variables.js
@@ -0,0 +1,23 @@
+let numero1=1;
+let numero2=2;
+numero1=5;
+numero2=8;
+numero1=true;
+numero2=false;
+
+const inmutable='lol';
+
+let numero=1;
+let flotante=2.1;
+let string="Miguel";
+let booleano=true;
+let objeto=null;
+let indefinido=undefined;
+
+
+console.log(typeof numero);
+console.log(typeof flotante);
+console.log(typeof string);
+console.log(typeof booleano);
+console.log(typeof objeto);
+console.log(typeof indefinido);
