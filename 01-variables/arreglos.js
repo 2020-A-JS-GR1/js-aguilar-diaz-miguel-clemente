131e30dbeff71cd3772e14b61eefede5715cb8c4
DekimXD <miguelclementeaguilardiaz@hotmail.com>
2020-06-19 16:55:08 -0500

    empezando operadores

--- 01-variables/arreglos.js
+++ 01-variables/arreglos.js
@@ -5,3 +5,20 @@ cualquierCosa=1;
 cualquierCosa=null;
 
 let arregloTodo=[true,'lol',1,null,undefined,{nombre:''}];
+
+for(let arr of arreglo){
+    console.log(arr)
+}
+
+for(let arr in arreglo){
+    console.log(arr)
+}
+
+arreglo.push(2);
+arreglo.unshift(30); // anade al principio
+arreglo.splice(0,1);
+arreglo.splice(0,0,3,4,5);
+console.log(arreglo.indexOf(5));
+
+
+console.log(arreglo)
