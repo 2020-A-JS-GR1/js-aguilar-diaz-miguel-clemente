8eae6878df75abc05fe204c6ea925779f46c6d54
DekimXD <miguelclementeaguilardiaz@hotmail.com>
2020-07-31 11:43:43 -0500

    correcciones

--- 02-typeScript/03-funciones.js
+++ 02-typeScript/03-funciones.js
@@ -1,7 +1,14 @@
-function suma(numeroInicial, ...numeros) {
+function suma(numeroInicial) {
+    var numeros = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        numeros[_i - 1] = arguments[_i];
+    }
     return numeroInicial;
 }
 console.log(suma(1, 2, 3, 4, 5));
 function imprimir(mensaje) {
-    return "el mensaje es: " + mensaje;
+    return 'el mensaje es: ' + mensaje;
 }
+// Sintaxis para arreglos
+var arregloNumeros = [1, 2, 3];
+var arregloNumerosDos = [1, 2, 3];
